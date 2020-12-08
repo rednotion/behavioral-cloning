078be271ae4bba9fa5959ed93b471907b083ff0f
elizabeth-dsaid <elizabeth@dsaid.gov.sg>
2020-12-08 19:47:55 +0800

    save loss progress fig

--- model.py
+++ model.py
@@ -5,9 +5,13 @@ import sklearn
 
 from random import shuffle
 from math import ceil
+import matplotlib.pyplot as plt
 
 from keras.models import Sequential
-from keras.layers import Cropping2D, Lambda
+from keras.layers import Cropping2D, Lambda, Flatten, Dense
+from keras.layers import Convolution2D
+
+model_name = 'test_model'
 
 data_path = '/opt/carnd_p3/data/'
 
@@ -78,7 +82,7 @@ model = Sequential()
 
 # Preprocess model
 model.add(Cropping2D(cropping=((crop_top, crop_bottom), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x / 127.5 - 1.,
+model.add(Lambda(lambda x: (x / 255.0) - 0.5,
                  input_shape=(n_channels, n_rows, n_cols),
                  output_shape=(n_channels, n_rows, n_cols)))
 
@@ -86,12 +90,22 @@ model.add(Lambda(lambda x: x / 127.5 - 1.,
 
 # Compile and train
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator,
-                    steps_per_epoch=ceil(len(train_samples) / batch_size),
-                    validation_data=validation_generator,
-                    validation_steps=ceil(len(validation_samples) / batch_size),
-                    epochs=n_epochs,
-                    verbose=1)
+training_hist = model.fit_generator(train_generator,
+                                    steps_per_epoch=ceil(len(train_samples) / batch_size),
+                                    validation_data=validation_generator,
+                                    validation_steps=ceil(len(validation_samples) / batch_size),
+                                    epochs=n_epochs,
+                                    verbose=1)
 
 # Save
-model.save('model.h5')
+model.save('{}.h5'.format(model_name))
+
+# Save training history as graph
+fig, ax = plt.subplots()
+plt.plot(training_hist.history['loss'])
+plt.plot(training_hist.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('{}_loss_progress.png'.format(model_name))
